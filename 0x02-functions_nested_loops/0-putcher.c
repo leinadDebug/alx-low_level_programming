3bec855e3a6eb079c38ab247f5d9b4aace6d1ccf
lumixAr12 <akujuaobidaniel@gmail.com>
2022-12-14 17:34:17 -0800

    task 1

---
+++ 0x02-functions_nested_loops/0-putcher.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/** 
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_putcher("_");
+	_putcher("p");
+	_putcher("u");
+	_putcher("t");
+	_putcher("c");
+	_putcher("h");
+	_putcher("e");
+	_putcher("r");
+	_putcher("\n");
+
+	return 0;
+}
