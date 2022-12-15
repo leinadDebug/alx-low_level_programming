5e5cb5b3539476a843ca1542d7215a48c1af6913
lumixAr12 <akujuaobidaniel@gmail.com>
2022-12-14 20:44:45 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * * print_alphabet_x10 - print all alphabet in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	_putchar('\n');
+	}
